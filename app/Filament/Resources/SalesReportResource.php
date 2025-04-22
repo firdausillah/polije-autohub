6061743bf2943a8a4ab02576650aae18f5b55fb9
firdausillah <firdausillah123@gmail.com>
2025-04-22 12:57:46 +0700

    update laporan penjualan sparepart

---
+++ app/Filament/Resources/SalesReportResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SalesReportResource\Pages;
+use App\Filament\Resources\SalesReportResource\RelationManagers;
+use App\Models\SalesReport;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+use function Laravel\Prompts\text;
+
+class SalesReportResource extends Resource
+{
+    protected static ?string $model = SalesReport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static ?string $navigationGroup = 'Laporan';
+
+    public static function getModelLabel(): string
+    {
+        return 'Laporan Penjualan';
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return 'Laporan Penjualan';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+
+                TextColumn::make('name')
+                ->searchable()
+                ->sortable()
+                ->toggleable(),
+                TextColumn::make('harga_modal')->money('IDR', locale: 'id_ID')->sortable()->toggleable(),
+                TextColumn::make('harga_jual')->money('IDR', locale: 'id_ID')->sortable()->toggleable(),
+                TextColumn::make('hpp')->money('IDR', locale: 'id_ID')->sortable()->toggleable(),
+                TextColumn::make('total_penjualan')->money('IDR', locale: 'id_ID')->sortable()->toggleable(),
+                TextColumn::make('laba_kotor')->money('IDR', locale: 'id_ID')->sortable()->toggleable(),
+                TextColumn::make('qty_terjual')->sortable()->toggleable(),
+                TextColumn::make('saldo')->sortable()->toggleable()
+                ->label('Saldo Barang'),
+                //
+            ])
+            ->filters([
+                //
+            // ])
+            // ->actions([
+            //     Tables\Actions\EditAction::make(),
+            // ])
+            // ->bulkActions([
+            //     Tables\Actions\BulkActionGroup::make([
+            //         Tables\Actions\DeleteBulkAction::make(),
+            //     ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSalesReports::route('/'),
+            // 'create' => Pages\CreateSalesReport::route('/create'),
+            // 'edit' => Pages\EditSalesReport::route('/{record}/edit'),
+        ];
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+}
