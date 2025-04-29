23e586ead307ca6f69b536c46a8d29d03ed93d7a
firdausillah <firdausillah123@gmail.com>
2025-04-29 20:53:20 +0700

    update service

--- app/Models/ServiceDSparepart.php
+++ app/Models/ServiceDSparepart.php
@@ -27,6 +27,12 @@ class ServiceDSparepart extends Model
             ->sum('pajak');
 
         $subTotal = $subTotalService + $subTotalSparepart;
+        dd(
+            $estimasi_waktu_pengerjaan,
+            $subTotalService,
+            $subTotalSparepart,
+            $totalPajak
+        );
 
         ServiceSchedule::find($this->service_schedule_id)
             ->update(['total' => $subTotal, 'service_total' => $subTotalService, 'sparepart_total' => $subTotalSparepart, 'total_estimasi_waktu' => $estimasi_waktu_pengerjaan, 'pajak_total' => $totalPajak]);
