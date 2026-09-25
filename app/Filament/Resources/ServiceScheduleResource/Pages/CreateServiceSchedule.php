e3d429fdceeb05cc97ad6d04c9f95408a3c00d6c
firdausillah <firdausillah123@gmail.com>
2026-09-25 22:09:32 +0700

    feat: add form data mutation to generate transaction code for service schedules

--- app/Filament/Resources/ServiceScheduleResource/Pages/CreateServiceSchedule.php
+++ app/Filament/Resources/ServiceScheduleResource/Pages/CreateServiceSchedule.php
@@ -3,10 +3,22 @@
 namespace App\Filament\Resources\ServiceScheduleResource\Pages;
 
 use App\Filament\Resources\ServiceScheduleResource;
+use App\Helpers\CodeGenerator;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateServiceSchedule extends CreateRecord
 {
     protected static string $resource = ServiceScheduleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['kode'] = CodeGenerator::generateTransactionCode(
+            'SSJ',
+            'service_schedules',
+            'kode'
+        );
+
+        return $data;
+    }
 }
